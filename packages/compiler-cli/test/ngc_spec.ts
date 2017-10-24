depthroot/angular

commit 21c933c231243322f7b9009e1d36a1bb52306d9c

refactor(compiler): remove old ngtools api and add listLazyRoutes to new api (#19836)
Usages of `NgTools_InternalApi_NG_2` from `@angular/compiler-cli` will now
throw an error.

Adds `listLazyRoutes` to `@angular/compiler-cli/ngtools2.ts` for getting
the lazy routes of a `ng.Program`.
PR Close #19836

packages/compiler-cli/src/transformers/program.ts
@@ -14,11 +14,12 @@ import * as ts from 'typescript';
 import {TypeCheckHost, translateDiagnostics} from '../diagnostics/translate_diagnostics';
 import {ModuleMetadata, createBundleIndexHost} from '../metadata/index';
 
-import {CompilerHost, CompilerOptions, CustomTransformers, DEFAULT_ERROR_CODE, Diagnostic, EmitFlags, LibrarySummary, Program, SOURCE, TsEmitArguments, TsEmitCallback} from './api';
+import {CompilerHost, CompilerOptions, CustomTransformers, DEFAULT_ERROR_CODE, Diagnostic, EmitFlags, LazyRoute, LibrarySummary, Program, SOURCE, TsEmitArguments, TsEmitCallback} from './api';
 import {CodeGenerator, TsCompilerAotCompilerTypeCheckHostAdapter, getOriginalReferences} from './compiler_host';
 import {LowerMetadataCache, getExpressionLoweringTransformFactory} from './lower_expressions';
 import {getAngularEmitterTransformFactory} from './node_emitter_transform';
 import {GENERATED_FILES, StructureIsReused, createMessageDiagnostic, tsStructureIsReused} from './util';
+
 
 /**
  * Maximum number of files that are emitable via calling ts.Program.emit
@@ -50,8 +51,8 @@ class AngularCompilerProgram implements Program {
   private emittedSourceFiles: ts.SourceFile[]|undefined;
 
   // Lazily initialized fields
-  private _typeCheckHost: TypeCheckHost;
   private _compiler: AotCompiler;
+  private _hostAdapter: TsCompilerAotCompilerTypeCheckHostAdapter;
   private _tsProgram: ts.Program;
   private _analyzedModules: NgAnalyzedModules|undefined;
   private _structuralDiagnostics: Diagnostic[]|undefined;
@@ -167,7 +168,7 @@ class AngularCompilerProgram implements Program {
         diags.push(...this.tsProgram.getSemanticDiagnostics(sf, cancellationToken));
       }
     });
-    const {ng} = translateDiagnostics(this.typeCheckHost, diags);
+    const {ng} = translateDiagnostics(this.hostAdapter, diags);
     return ng;
   }
 
@@ -175,16 +176,21 @@ class AngularCompilerProgram implements Program {
     if (this._analyzedModules) {
       throw new Error('Angular structure already loaded');
     }
-    const {tmpProgram, sourceFiles, hostAdapter, rootNames} = this._createProgramWithBasicStubs();
-    return this._compiler.loadFilesAsync(sourceFiles)
+    const {tmpProgram, sourceFiles, rootNames} = this._createProgramWithBasicStubs();
+    return this.compiler.loadFilesAsync(sourceFiles)
         .catch(this.catchAnalysisError.bind(this))
         .then(analyzedModules => {
           if (this._analyzedModules) {
             throw new Error('Angular structure loaded both synchronously and asynchronsly');
           }
-          this._updateProgramWithTypeCheckStubs(
-              tmpProgram, analyzedModules, hostAdapter, rootNames);
+          this._updateProgramWithTypeCheckStubs(tmpProgram, analyzedModules, rootNames);
         });
+  }
+
+  listLazyRoutes(route?: string): LazyRoute[] {
+    // Note: Don't analyzedModules if a route is given
+    // to be fast enough.
+    return this.compiler.listLazyRoutes(route, route ? undefined : this.analyzedModules);
   }
 
   emit(
@@ -341,9 +347,16 @@ class AngularCompilerProgram implements Program {
   // Private members
   private get compiler(): AotCompiler {
     if (!this._compiler) {
-      this.initSync();
+      this._createCompiler();
     }
     return this._compiler !;
+  }
+
+  private get hostAdapter(): TsCompilerAotCompilerTypeCheckHostAdapter {
+    if (!this._hostAdapter) {
+      this._createCompiler();
+    }
+    return this._hostAdapter !;
   }
 
   private get analyzedModules(): NgAnalyzedModules {
@@ -367,13 +380,6 @@ class AngularCompilerProgram implements Program {
     return this._tsProgram !;
   }
 
-  private get typeCheckHost(): TypeCheckHost {
-    if (!this._typeCheckHost) {
-      this.initSync();
-    }
-    return this._typeCheckHost !;
-  }
-
   private calculateTransforms(
       genFiles: Map<string, GeneratedFile>,
       customTransformers?: CustomTransformers): ts.CustomTransformers {
@@ -393,19 +399,41 @@ class AngularCompilerProgram implements Program {
     if (this._analyzedModules) {
       return;
     }
-    const {tmpProgram, sourceFiles, hostAdapter, rootNames} = this._createProgramWithBasicStubs();
+    const {tmpProgram, sourceFiles, rootNames} = this._createProgramWithBasicStubs();
     let analyzedModules: NgAnalyzedModules|null;
     try {
-      analyzedModules = this._compiler.loadFilesSync(sourceFiles);
+      analyzedModules = this.compiler.loadFilesSync(sourceFiles);
     } catch (e) {
       analyzedModules = this.catchAnalysisError(e);
     }
-    this._updateProgramWithTypeCheckStubs(tmpProgram, analyzedModules, hostAdapter, rootNames);
+    this._updateProgramWithTypeCheckStubs(tmpProgram, analyzedModules, rootNames);
+  }
+
+  private _createCompiler() {
+    const codegen: CodeGenerator = {
+      generateFile: (genFileName, baseFileName) =>
+                        this._compiler.emitBasicStub(genFileName, baseFileName),
+      findGeneratedFileNames: (fileName) => this._compiler.findGeneratedFileNames(fileName),
+    };
+
+    this._hostAdapter = new TsCompilerAotCompilerTypeCheckHostAdapter(
+        this.rootNames, this.options, this.host, this.metadataCache, codegen,
+        this.oldProgramLibrarySummaries);
+    const aotOptions = getAotCompilerOptions(this.options);
+    this._structuralDiagnostics = [];
+    const errorCollector = (err: any) => {
+      this._structuralDiagnostics !.push({
+        messageText: err.toString(),
+        category: ts.DiagnosticCategory.Error,
+        source: SOURCE,
+        code: DEFAULT_ERROR_CODE
+      });
+    };
+    this._compiler = createAotCompiler(this._hostAdapter, aotOptions, errorCollector).compiler;
   }
 
   private _createProgramWithBasicStubs(): {
     tmpProgram: ts.Program,
-    hostAdapter: TsCompilerAotCompilerTypeCheckHostAdapter,
     rootNames: string[],
     sourceFiles: string[],
   } {
@@ -418,58 +446,56 @@ class AngularCompilerProgram implements Program {
 
     const codegen: CodeGenerator = {
       generateFile: (genFileName, baseFileName) =>
-                        this._compiler.emitBasicStub(genFileName, baseFileName),
-      findGeneratedFileNames: (fileName) => this._compiler.findGeneratedFileNames(fileName),
+                        this.compiler.emitBasicStub(genFileName, baseFileName),
+      findGeneratedFileNames: (fileName) => this.compiler.findGeneratedFileNames(fileName),
     };
 
-    const hostAdapter = new TsCompilerAotCompilerTypeCheckHostAdapter(
-        this.rootNames, this.options, this.host, this.metadataCache, codegen,
-        this.oldProgramLibrarySummaries);
-    const aotOptions = getAotCompilerOptions(this.options);
-    this._compiler = createAotCompiler(hostAdapter, aotOptions).compiler;
-    this._typeCheckHost = hostAdapter;
-    this._structuralDiagnostics = [];
 
-    let rootNames =
-        this.rootNames.filter(fn => !GENERATED_FILES.test(fn) || !hostAdapter.isSourceFile(fn));
+    let rootNames = this.rootNames;
+    if (this.options.generateCodeForLibraries !== false) {
+      // if we should generateCodeForLibraries, enver include
+      // generated files in the program as otherwise we will
+      // ovewrite them and typescript will report the error
+      // TS5055: Cannot write file ... because it would overwrite input file.
+      rootNames = this.rootNames.filter(fn => !GENERATED_FILES.test(fn));
+    }
     if (this.options.noResolve) {
       this.rootNames.forEach(rootName => {
-        if (hostAdapter.shouldGenerateFilesFor(rootName)) {
-          rootNames.push(...this._compiler.findGeneratedFileNames(rootName));
+        if (this.hostAdapter.shouldGenerateFilesFor(rootName)) {
+          rootNames.push(...this.compiler.findGeneratedFileNames(rootName));
         }
       });
     }
 
-    const tmpProgram = ts.createProgram(rootNames, this.options, hostAdapter, oldTsProgram);
+    const tmpProgram = ts.createProgram(rootNames, this.options, this.hostAdapter, oldTsProgram);
     const sourceFiles: string[] = [];
     tmpProgram.getSourceFiles().forEach(sf => {
-      if (hostAdapter.isSourceFile(sf.fileName)) {
+      if (this.hostAdapter.isSourceFile(sf.fileName)) {
         sourceFiles.push(sf.fileName);
       }
     });
-    return {tmpProgram, sourceFiles, hostAdapter, rootNames};
+    return {tmpProgram, sourceFiles, rootNames};
   }
 
   private _updateProgramWithTypeCheckStubs(
-      tmpProgram: ts.Program, analyzedModules: NgAnalyzedModules|null,
-      hostAdapter: TsCompilerAotCompilerTypeCheckHostAdapter, rootNames: string[]) {
+      tmpProgram: ts.Program, analyzedModules: NgAnalyzedModules|null, rootNames: string[]) {
     this._analyzedModules = analyzedModules || emptyModules;
     if (analyzedModules) {
       tmpProgram.getSourceFiles().forEach(sf => {
         if (sf.fileName.endsWith('.ngfactory.ts')) {
-          const {generate, baseFileName} = hostAdapter.shouldGenerateFile(sf.fileName);
+          const {generate, baseFileName} = this.hostAdapter.shouldGenerateFile(sf.fileName);
           if (generate) {
             // Note: ! is ok as hostAdapter.shouldGenerateFile will always return a basefileName
             // for .ngfactory.ts files.
-            const genFile = this._compiler.emitTypeCheckStub(sf.fileName, baseFileName !);
+            const genFile = this.compiler.emitTypeCheckStub(sf.fileName, baseFileName !);
             if (genFile) {
-              hostAdapter.updateGeneratedFile(genFile);
+              this.hostAdapter.updateGeneratedFile(genFile);
             }
           }
         }
       });
     }
-    this._tsProgram = ts.createProgram(rootNames, this.options, hostAdapter, tmpProgram);
+    this._tsProgram = ts.createProgram(rootNames, this.options, this.hostAdapter, tmpProgram);
     // Note: the new ts program should be completely reusable by TypeScript as:
     // - we cache all the files in the hostAdapter
     // - new new stubs use the exactly same imports/exports as the old once (we assert that in

packages/compiler-cli/index.ts
@@ -6,13 +6,10 @@
  * found in the LICENSE file at https://angular.io/license
  */
 export {AotCompilerHost, AotCompilerHost as StaticReflectorHost, StaticReflector, StaticSymbol} from '@angular/compiler';
-export {CodeGenerator} from './src/codegen';
-export {CompilerHost, CompilerHostContext, ModuleResolutionHostAdapter, NodeCompilerHostContext} from './src/compiler_host';
 export {DiagnosticTemplateInfo, getExpressionScope, getTemplateExpressionDiagnostics} from './src/diagnostics/expression_diagnostics';
 export {AstType, ExpressionDiagnosticsContext} from './src/diagnostics/expression_type';
 export {BuiltinType, DeclarationKind, Definition, PipeInfo, Pipes, Signature, Span, Symbol, SymbolDeclaration, SymbolQuery, SymbolTable} from './src/diagnostics/symbols';
 export {getClassMembersFromDeclaration, getPipesTable, getSymbolQuery} from './src/diagnostics/typescript_symbols';
-export {Extractor} from './src/extractor';
 export {VERSION} from './src/version';
 
 export * from './src/metadata';
@@ -21,8 +18,7 @@ export * from './src/transformers/entry_points';
 
 export * from './src/perform_compile';
 
-// TODO(tbosch): remove this once everyone is on transformers
+// TODO(tbosch): remove this once cli 1.5 is fully released,
+// and usages in G3 are changed to `CompilerOptions`.
 export {CompilerOptions as AngularCompilerOptions} from './src/transformers/api';
-
-// TODO(hansl): moving to Angular 4 need to update this API.
 export {NgTools_InternalApi_NG_2 as __NGTOOLS_PRIVATE_API_2} from './src/ngtools_api';

packages/compiler-cli/integrationtest/test/test_ngtools_api.ts
@@ -15,8 +15,6 @@ import * as ts from 'typescript';
 import * as assert from 'assert';
 import {__NGTOOLS_PRIVATE_API_2, readConfiguration} from '@angular/compiler-cli';
 
-const glob = require('glob');
-
 /* tslint:disable:no-console  */
 /**
  * Main method.
@@ -27,9 +25,6 @@ function main() {
   console.log(`testing ngtools API...`);
 
   Promise.resolve()
-      .then(() => codeGenTest())
-      .then(() => codeGenTest(true))
-      .then(() => i18nTest())
       .then(() => lazyRoutesTest())
       .then(() => {
         console.log('All done!');
@@ -39,152 +34,6 @@ function main() {
         console.error(err.stack);
         console.error('Test failed');
         process.exit(1);
-      });
-}
-
-function codeGenTest(forceError = false) {
-  const basePath = path.join(__dirname, '../ngtools_src');
-  const srcPath = path.join(__dirname, '../src');
-  const project = path.join(basePath, 'tsconfig-build.json');
-  const readResources: string[] = [];
-  const wroteFiles: string[] = [];
-
-  const config = readConfiguration(project);
-  const delegateHost = ts.createCompilerHost(config.options, true);
-  const host: ts.CompilerHost = Object.assign(
-      {}, delegateHost,
-      {writeFile: (fileName: string, ...rest: any[]) => { wroteFiles.push(fileName); }});
-  const program = ts.createProgram(config.rootNames, config.options, host);
-
-  config.options.basePath = basePath;
-
-  console.log(`>>> running codegen for ${project}`);
-  if (forceError) {
-    console.log(`>>> asserting that missingTranslation param with error value throws`);
-  }
-  return __NGTOOLS_PRIVATE_API_2
-      .codeGen({
-        basePath,
-        compilerOptions: config.options, program, host,
-
-        angularCompilerOptions: config.options,
-
-        // i18n options.
-        i18nFormat: 'xlf',
-        i18nFile: path.join(srcPath, 'messages.fi.xlf'),
-        locale: 'fi',
-        missingTranslation: forceError ? 'error' : 'ignore',
-
-        readResource: (fileName: string) => {
-          readResources.push(fileName);
-          if (!host.fileExists(fileName)) {
-            throw new Error(`Compilation failed. Resource file not found: ${fileName}`);
-          }
-          return Promise.resolve(host.readFile(fileName));
-        }
-      })
-      .then(() => {
-        console.log(`>>> codegen done, asserting read and wrote files`);
-
-        // Assert for each file that it has been read and each `ts` has a written file associated.
-        const allFiles = glob.sync(path.join(basePath, '**/*'), {nodir: true});
-
-        allFiles.forEach((fileName: string) => {
-          // Skip tsconfig.
-          if (fileName.match(/tsconfig-build.json$/)) {
-            return;
-          }
-
-          // Assert that file was read.
-          if (fileName.match(/\.module\.ts$/)) {
-            const factory = fileName.replace(/\.module\.ts$/, '.module.ngfactory.ts');
-            assert(wroteFiles.indexOf(factory) != -1, `Expected file "${factory}" to be written.`);
-          } else if (fileName.match(/\.css$/) || fileName.match(/\.html$/)) {
-            assert(
-                readResources.indexOf(fileName) != -1,
-                `Expected resource "${fileName}" to be read.`);
-          }
-        });
-
-        console.log(`done, no errors.`);
-      })
-      .catch((e: Error) => {
-        if (forceError) {
-          assert(
-              e.message.match(`Missing translation for message`),
-              `Expected error message for missing translations`);
-          console.log(`done, error catched`);
-        } else {
-          console.error(e.stack);
-          console.error('Compilation failed');
-          throw e;
-        }
-      });
-}
-
-function i18nTest() {
-  const basePath = path.join(__dirname, '../ngtools_src');
-  const project = path.join(basePath, 'tsconfig-build.json');
-  const readResources: string[] = [];
-  const wroteFiles: string[] = [];
-
-  const config = readConfiguration(project);
-  const delegateHost = ts.createCompilerHost(config.options, true);
-  const host: ts.CompilerHost = Object.assign(
-      {}, delegateHost,
-      {writeFile: (fileName: string, ...rest: any[]) => { wroteFiles.push(fileName); }});
-  const program = ts.createProgram(config.rootNames, config.options, host);
-
-  config.options.basePath = basePath;
-
-  console.log(`>>> running i18n extraction for ${project}`);
-  return __NGTOOLS_PRIVATE_API_2
-      .extractI18n({
-        basePath,
-        compilerOptions: config.options, program, host,
-        angularCompilerOptions: config.options,
-        i18nFormat: 'xlf',
-        locale: undefined,
-        outFile: undefined,
-        readResource: (fileName: string) => {
-          readResources.push(fileName);
-          if (!host.fileExists(fileName)) {
-            throw new Error(`Compilation failed. Resource file not found: ${fileName}`);
-          }
-          return Promise.resolve(host.readFile(fileName));
-        },
-      })
-      .then(() => {
-        console.log(`>>> i18n extraction done, asserting read and wrote files`);
-
-        const allFiles = glob.sync(path.join(basePath, '**/*'), {nodir: true});
-
-        assert(wroteFiles.length == 1, `Expected a single message bundle file.`);
-
-        assert(
-            wroteFiles[0].endsWith('/ngtools_src/messages.xlf'),
-            `Expected the bundle file to be "message.xlf".`);
-
-        allFiles.forEach((fileName: string) => {
-          // Skip tsconfig.
-          if (fileName.match(/tsconfig-build.json$/)) {
-            return;
-          }
-
-          // Assert that file was read.
-          if (fileName.match(/\.css$/) || fileName.match(/\.html$/)) {
-            assert(
-                readResources.indexOf(fileName) != -1,
-                `Expected resource "${fileName}" to be read.`);
-          }
-        });
-
-        console.log(`done, no errors.`);
-      })
-      .catch((e: Error) => {
-        console.error(e.stack);
-        console.error('Extraction failed');
-        throw e;
       });
 }
 

packages/compiler-cli/integrationtest/test/test_summaries.ts
@@ -1,126 +1,0 @@
-#!/usr/bin/env node
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-
-// Must be imported first, because Angular decorators throw on load.
-import 'reflect-metadata';
-
-import * as path from 'path';
-import * as ts from 'typescript';
-import * as assert from 'assert';
-import {CompilerOptions, CodeGenerator, CompilerHostContext, NodeCompilerHostContext, readConfiguration} from '@angular/compiler-cli';
-
-/* tslint:disable:no-console  */
-/**
- * Main method.
- * Standalone program that executes the real codegen and tests that
- * ngsummary.json files are used for libraries.
- */
-function main() {
-  console.log(`testing usage of ngsummary.json files in libraries...`);
-  const basePath = path.resolve(__dirname, '..');
-  const project = path.resolve(basePath, 'tsconfig-build.json');
-  const readFiles: string[] = [];
-  const writtenFiles: {fileName: string, content: string}[] = [];
-
-  class AssertingHostContext extends NodeCompilerHostContext {
-    readFile(fileName: string): string {
-      if (/.*\/node_modules\/.*/.test(fileName) && !/.*ngsummary\.json$/.test(fileName) &&
-          !/package\.json$/.test(fileName)) {
-        // Only allow to read summaries and package.json files from node_modules
-        // TODO (mhevery): Fix this. TypeScript.d.ts does not allow returning null.
-        return null !;
-      }
-      readFiles.push(path.relative(basePath, fileName));
-      return super.readFile(fileName);
-    }
-    readResource(s: string): Promise<string> {
-      readFiles.push(path.relative(basePath, s));
-      return super.readResource(s);
-    }
-  }
-
-  const config = readConfiguration(project);
-  config.options.basePath = basePath;
-  // This flag tells ngc do not recompile libraries.
-  config.options.generateCodeForLibraries = false;
-
-  console.log(`>>> running codegen for ${project}`);
-  codegen(
-      config,
-      (host) => {
-        host.writeFile = (fileName: string, content: string) => {
-          fileName = path.relative(basePath, fileName);
-          writtenFiles.push({fileName, content});
-        };
-        return new AssertingHostContext();
-      })
-      .then((exitCode: any) => {
-        console.log(`>>> codegen done, asserting read files`);
-        assertSomeFileMatch(readFiles, /^node_modules\/.*\.ngsummary\.json$/);
-        assertNoFileMatch(readFiles, /^node_modules\/.*\.metadata.json$/);
-        assertNoFileMatch(readFiles, /^node_modules\/.*\.html$/);
-        assertNoFileMatch(readFiles, /^node_modules\/.*\.css$/);
-
-        assertNoFileMatch(readFiles, /^src\/.*\.ngsummary\.json$/);
-        assertSomeFileMatch(readFiles, /^src\/.*\.html$/);
-        assertSomeFileMatch(readFiles, /^src\/.*\.css$/);
-
-        console.log(`>>> asserting written files`);
-        assertWrittenFile(writtenFiles, /^src\/module\.ngfactory\.ts$/, /class MainModuleInjector/);
-
-        console.log(`done, no errors.`);
-        process.exit(exitCode);
-      })
-      .catch((e: any) => {
-        console.error(e.stack);
-        console.error('Compilation failed');
-        process.exit(1);
-      });
-}
-
-/**
- * Simple adaption of main to just run codegen with a CompilerHostContext
- */
-function codegen(
-    config: {options: CompilerOptions, rootNames: string[]},
-    hostContextFactory: (host: ts.CompilerHost) => CompilerHostContext) {
-  const host = ts.createCompilerHost(config.options, true);
-
-  // HACK: patch the realpath to solve symlink issue here:
-  // https://github.com/Microsoft/TypeScript/issues/9552
-  // todo(misko): remove once facade symlinks are removed
-  host.realpath = (path) => path;
-
-  const program = ts.createProgram(config.rootNames, config.options, host);
-
-  return CodeGenerator.create(config.options, {
-                      } as any, program, host, hostContextFactory(host)).codegen();
-}
-
-function assertSomeFileMatch(fileNames: string[], pattern: RegExp) {
-  assert(
-      fileNames.some(fileName => pattern.test(fileName)),
-      `Expected some read files match ${pattern}`);
-}
-
-function assertNoFileMatch(fileNames: string[], pattern: RegExp) {
-  const matches = fileNames.filter(fileName => pattern.test(fileName));
-  assert(
-      matches.length === 0,
-      `Expected no read files match ${pattern}, but found: \n${matches.join('\n')}`);
-}
-
-function assertWrittenFile(
-    files: {fileName: string, content: string}[], filePattern: RegExp, contentPattern: RegExp) {
-  assert(
-      files.some(file => filePattern.test(file.fileName) && contentPattern.test(file.content)),
-      `Expected some written files for ${filePattern} and content ${contentPattern}`);
-}
-
-main();

commit 5028f528d4590a5e056febdab5df757633b4d4eb

fix(compiler-cli): report all diagnostic error messages (#19886)
This fixes a problem introduced in 21c933c231243322f7b9009e1d36a1bb52306d9c
which modified how diagnostic error messages are reported for structural
metadata errors causing some of the diagnostics to be lost.

PR Close #19886

packages/compiler-cli/src/transformers/program.ts
@@ -510,21 +510,25 @@ class AngularCompilerProgram implements Program {
     if (isSyntaxError(e)) {
       const parserErrors = getParseErrors(e);
       if (parserErrors && parserErrors.length) {
-        this._structuralDiagnostics =
-            parserErrors.map<Diagnostic>(e => ({
-                                           messageText: e.contextualMessage(),
-                                           category: ts.DiagnosticCategory.Error,
-                                           span: e.span,
-                                           source: SOURCE,
-                                           code: DEFAULT_ERROR_CODE
-                                         }));
+        this._structuralDiagnostics = [
+          ...(this._structuralDiagnostics || []),
+          ...parserErrors.map<Diagnostic>(e => ({
+                                            messageText: e.contextualMessage(),
+                                            category: ts.DiagnosticCategory.Error,
+                                            span: e.span,
+                                            source: SOURCE,
+                                            code: DEFAULT_ERROR_CODE
+                                          }))
+        ];
       } else {
-        this._structuralDiagnostics = [{
-          messageText: e.message,
-          category: ts.DiagnosticCategory.Error,
-          source: SOURCE,
-          code: DEFAULT_ERROR_CODE
-        }];
+        this._structuralDiagnostics = [
+          ...(this._structuralDiagnostics || []), {
+            messageText: e.message,
+            category: ts.DiagnosticCategory.Error,
+            source: SOURCE,
+            code: DEFAULT_ERROR_CODE
+          }
+        ];
       }
       return null;
     }

packages/compiler-cli/test/ngc_spec.ts
@@ -1391,5 +1391,44 @@ describe('ngc transformer command-line', () => {
           main(['-p', path.join(basePath, 'src/tsconfig.json')], message => messages.push(message));
       expect(exitCode).toBe(0, 'Compile failed unexpectedly.\n  ' + messages.join('\n  '));
     });
+
+    it('should emit all structural errors', () => {
+      write('src/tsconfig.json', `{
+        "extends": "../tsconfig-base.json",
+        "files": ["test-module.ts"]
+      }`);
+      write('src/lib/indirect2.ts', `
+        declare var f: any;
+        export const t2 = f\`<p>hello</p>\`;
+      `);
+      write('src/lib/indirect1.ts', `
+        import {t2} from './indirect2';
+        export const t1 = t2 + ' ';
+      `);
+      write('src/lib/test.component.ts', `
+        import {Component} from '@angular/core';
+        import {t1} from './indirect1';
+
+        @Component({
+          template: t1
+        })
+        export class TestComponent {}
+      `);
+      write('src/test-module.ts', `
+        import {NgModule} from '@angular/core';
+        import {TestComponent} from './lib/test.component';
+
+        @NgModule({declarations: [TestComponent]})
+        export class TestModule {}
+      `);
+      const messages: string[] = [];
+      const exitCode =
+          main(['-p', path.join(basePath, 'src/tsconfig.json')], message => messages.push(message));
+      expect(exitCode).toBe(1, 'Compile was expected to fail');
+      expect(messages).toEqual([
+        'Error: Error: Error encountered resolving symbol values statically. Tagged template expressions are not supported in metadata (position 3:27 in the original .ts file)\n' +
+        'Error: No template specified for component TestComponent\n'
+      ]);
+    });
   });
 });