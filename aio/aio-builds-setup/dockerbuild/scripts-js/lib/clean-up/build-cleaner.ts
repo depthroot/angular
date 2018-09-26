depthroot/angular

commit da74baea9a5dcf216a457a126bfe418328a2cf41

ci(docs-infra): change AIO preview server stuff to pull builds from CircleCI
Previously, Travis pushed the build artitfacts to the preview server.
This required us to use JWT to secure the POST request from Travis, to
ensure we couldn't receive malicious builds.

JWT has been deprecated and we are moving our builds to CircleCI.

This commit rewrites the TypeScript part of the preview server that
handles converting build artifact into hosted previews of the docs.

aio/aio-builds-setup/dockerbuild/scripts-js/lib/clean-up/build-cleaner.ts
@@ -3,29 +3,51 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as shell from 'shelljs';
 import {HIDDEN_DIR_PREFIX} from '../common/constants';
+import {GithubApi} from '../common/github-api';
 import {GithubPullRequests} from '../common/github-pull-requests';
-import {assertNotMissingOrEmpty} from '../common/utils';
+import {assertNotMissingOrEmpty, getPrInfoFromDownloadPath} from '../common/utils';
 
 // Classes
 export class BuildCleaner {
+
   // Constructor
-  constructor(protected buildsDir: string, protected repoSlug: string, protected githubToken: string) {
+  constructor(protected buildsDir: string, protected githubOrg: string, protected githubRepo: string,
+              protected githubToken: string, protected downloadsDir: string, protected artifactPath: string) {
     assertNotMissingOrEmpty('buildsDir', buildsDir);
-    assertNotMissingOrEmpty('repoSlug', repoSlug);
+    assertNotMissingOrEmpty('githubOrg', githubOrg);
+    assertNotMissingOrEmpty('githubRepo', githubRepo);
     assertNotMissingOrEmpty('githubToken', githubToken);
+    assertNotMissingOrEmpty('downloadsDir', downloadsDir);
+    assertNotMissingOrEmpty('artifactPath', artifactPath);
   }
 
   // Methods - Public
-  public cleanUp(): Promise<void> {
-    return Promise.all([
-      this.getExistingBuildNumbers(),
-      this.getOpenPrNumbers(),
-    ]).then(([existingBuilds, openPrs]) => this.removeUnnecessaryBuilds(existingBuilds, openPrs));
+  public async cleanUp() {
+    try {
+      this.logger.log('Cleaning up builds and downloads');
+      const openPrs = await this.getOpenPrNumbers();
+      this.logger.log(`Open pull requests: ${openPrs.length}`);
+      await Promise.all([
+        this.cleanBuilds(openPrs),
+        this.cleanDownloads(openPrs),
+      ]);
+    } catch (error) {
+      this.logger.error('ERROR:', error);
+    }
   }
 
-  // Methods - Protected
-  protected getExistingBuildNumbers(): Promise<number[]> {
-    return new Promise((resolve, reject) => {
+  public async cleanBuilds(openPrs: number[]) {
+    const existingBuilds = await this.getExistingBuildNumbers();
+    await this.removeUnnecessaryBuilds(existingBuilds, openPrs);
+  }
+
+  public async cleanDownloads(openPrs: number[]) {
+    const existingDownloads = await this.getExistingDownloads();
+    await this.removeUnnecessaryDownloads(existingDownloads, openPrs);
+  }
+
+  public getExistingBuildNumbers() {
+    return new Promise<number[]>((resolve, reject) => {
       fs.readdir(this.buildsDir, (err, files) => {
         if (err) {
           return reject(err);
@@ -41,15 +63,14 @@ export class BuildCleaner {
     });
   }
 
-  protected getOpenPrNumbers(): Promise<number[]> {
-    const githubPullRequests = new GithubPullRequests(this.githubToken, this.repoSlug);
-
-    return githubPullRequests.
-      fetchAll('open').
-      then(prs => prs.map(pr => pr.number));
+  public async getOpenPrNumbers() {
+    const api = new GithubApi(this.githubToken);
+    const githubPullRequests = new GithubPullRequests(api, this.githubOrg, this.githubRepo);
+    const prs = await githubPullRequests.fetchAll('open');
+    return prs.map(pr => pr.number);
   }
 
-  protected removeDir(dir: string) {
+  public removeDir(dir: string) {
     try {
       if (shell.test('-d', dir)) {
         shell.chmod('-R', 'a+w', dir);
@@ -60,11 +81,10 @@ export class BuildCleaner {
     }
   }
 
-  protected removeUnnecessaryBuilds(existingBuildNumbers: number[], openPrNumbers: number[]) {
+  public removeUnnecessaryBuilds(existingBuildNumbers: number[], openPrNumbers: number[]) {
     const toRemove = existingBuildNumbers.filter(num => !openPrNumbers.includes(num));
 
     console.log(`Existing builds: ${existingBuildNumbers.length}`);
-    console.log(`Open pull requests: ${openPrNumbers.length}`);
     console.log(`Removing ${toRemove.length} build(s): ${toRemove.join(', ')}`);
 
     // Try removing public dirs.
@@ -76,5 +96,30 @@ export class BuildCleaner {
     toRemove.
       map(num => path.join(this.buildsDir, HIDDEN_DIR_PREFIX + String(num))).
       forEach(dir => this.removeDir(dir));
+  }
+
+  public getExistingDownloads() {
+    const artifactFile = path.basename(this.artifactPath);
+    return new Promise<string[]>((resolve, reject) => {
+      fs.readdir(this.downloadsDir, (err, files) => {
+        if (err) {
+          return reject(err);
+        }
+        files = files.filter(file => file.endsWith(artifactFile));
+        resolve(files);
+      });
+    });
+  }
+
+  public removeUnnecessaryDownloads(existingDownloads: string[], openPrNumbers: number[]) {
+    const toRemove = existingDownloads.filter(filePath => {
+      const {pr} = getPrInfoFromDownloadPath(filePath);
+      return !openPrNumbers.includes(pr);
+    });
+
+    console.log(`Existing downloads: ${existingDownloads.length}`);
+    console.log(`Removing ${toRemove.length} download(s): ${toRemove.join(', ')}`);
+
+    toRemove.forEach(filePath => shell.rm(filePath));
   }
 }

commit 3313301d01bddcb807ed28524b07e5573e38ab5f

fix(docs-infra): fix preview server periodic clean-up (#25671)
Includes the following fixes:

- Fix cron entry format for clean-up script.
  Crontabs in `/etc` should not have a user field. No idea why it used
  to work before, but it started giving errors recently:
  `/bin/sh: root: not found`.

- Set required env variable in clean-up script. (Broken in da74baea9.)
  This was producing the following error:
  `ERROR: Missing required environment variable 'AIO_CIRCLE_CI_TOKEN'!`

- Use the correct path for downloads to be removed. (Broken in da74baea9.)

PR Close #25671

aio/aio-builds-setup/dockerbuild/cronjobs/aio-builds-cleanup
@@ -1,2 +1,2 @@
 # Periodically clean up builds that do not correspond to currently open PRs
-0 12 * * * root /usr/local/bin/aio-clean-up >> /var/log/cron.log 2>&1
+0 12 * * * /usr/local/bin/aio-clean-up >> /var/log/cron.log 2>&1

aio/aio-builds-setup/dockerbuild/scripts-js/lib/clean-up/build-cleaner.ts
@@ -122,6 +122,6 @@ export class BuildCleaner {
     this.logger.log(`Existing downloads: ${existingDownloads.length}`);
     this.logger.log(`Removing ${toRemove.length} download(s): ${toRemove.join(', ')}`);
 
-    toRemove.forEach(filePath => shell.rm(filePath));
+    toRemove.forEach(filePath => shell.rm(path.join(this.downloadsDir, filePath)));
   }
 }

aio/aio-builds-setup/dockerbuild/scripts-sh/clean-up.sh
@@ -2,6 +2,7 @@
 set -eu -o pipefail
 
 # Set up env variables
+export AIO_CIRCLE_CI_TOKEN=UNUSED_CIRCLE_CI_TOKEN
 export AIO_GITHUB_TOKEN=$(head -c -1 /aio-secrets/GITHUB_TOKEN 2>/dev/null)
 
 # Run the clean-up

aio/aio-builds-setup/dockerbuild/scripts-js/test/clean-up/build-cleaner.spec.ts
@@ -9,10 +9,10 @@ import {Logger} from '../../lib/common/utils';
 
 const EXISTING_BUILDS = [10, 20, 30, 40];
 const EXISTING_DOWNLOADS = [
-  'downloads/10-ABCDEF0-build.zip',
-  'downloads/10-1234567-build.zip',
-  'downloads/20-ABCDEF0-build.zip',
-  'downloads/20-1234567-build.zip',
+  '10-ABCDEF0-build.zip',
+  '10-1234567-build.zip',
+  '20-ABCDEF0-build.zip',
+  '20-1234567-build.zip',
 ];
 const OPEN_PRS = [10, 40];
 const ANY_DATE = jasmine.any(String);
@@ -26,7 +26,7 @@ describe('BuildCleaner', () => {
   beforeEach(() => {
     loggerErrorSpy = spyOn(Logger.prototype, 'error');
     loggerLogSpy = spyOn(Logger.prototype, 'log');
-    cleaner = new BuildCleaner('/foo/bar', 'baz', 'qux', '12345', 'downloads', 'build.zip');
+    cleaner = new BuildCleaner('/foo/bar', 'baz', 'qux', '12345', '/downloads', 'build.zip');
   });
 
   describe('constructor()', () => {
@@ -54,10 +54,12 @@ describe('BuildCleaner', () => {
         toThrowError('Missing or empty required parameter \'githubToken\'!');
     });
 
+
     it('should throw if \'downloadsDir\' is empty', () => {
       expect(() => new BuildCleaner('/foo/bar', 'baz', 'qux', '12345', '', 'build.zip')).
         toThrowError('Missing or empty required parameter \'downloadsDir\'!');
     });
+
 
     it('should throw if \'artifactPath\' is empty', () => {
       expect(() => new BuildCleaner('/foo/bar', 'baz', 'qux', '12345', 'downloads', '')).
@@ -166,6 +168,7 @@ describe('BuildCleaner', () => {
       }
     });
 
+
     it('should reject if \'removeUnnecessaryDownloads()\' rejects', async () => {
       try {
         cleanerRemoveUnnecessaryDownloadsSpy.and.callFake(() => Promise.reject('Test'));
@@ -174,6 +177,7 @@ describe('BuildCleaner', () => {
         expect(err).toBe('Test');
       }
     });
+
   });
 
 
@@ -283,12 +287,14 @@ describe('BuildCleaner', () => {
       prDeferred.resolve([{id: 0, number: 1}, {id: 1, number: 2}, {id: 2, number: 3}]);
     });
 
+
     it('should log the number of open PRs', () => {
       promise.then(prNumbers => {
         expect(loggerLogSpy).toHaveBeenCalledWith(
           ANY_DATE, 'BuildCleaner:        ', `Open pull requests: ${prNumbers}`);
       });
     });
+
   });
 
 
@@ -308,9 +314,9 @@ describe('BuildCleaner', () => {
     });
 
 
-    it('should get the contents of the builds directory', () => {
+    it('should get the contents of the downloads directory', () => {
       expect(fsReaddirSpy).toHaveBeenCalled();
-      expect(fsReaddirSpy.calls.argsFor(0)[0]).toBe('downloads');
+      expect(fsReaddirSpy.calls.argsFor(0)[0]).toBe('/downloads');
     });
 
 
@@ -324,7 +330,7 @@ describe('BuildCleaner', () => {
     });
 
 
-    it('should resolve with the returned files (as numbers)', done => {
+    it('should resolve with the returned file names', done => {
       promise.then(result => {
         expect(result).toEqual(EXISTING_DOWNLOADS);
         done();
@@ -460,25 +466,36 @@ describe('BuildCleaner', () => {
 
 
   describe('removeUnnecessaryDownloads()', () => {
+    let shellRmSpy: jasmine.Spy;
+
     beforeEach(() => {
-      spyOn(shell, 'rm');
+      shellRmSpy = spyOn(shell, 'rm');
+    });
+
+
+    it('should log the number of existing downloads and downloads to be removed', () => {
+      cleaner.removeUnnecessaryDownloads(EXISTING_DOWNLOADS, OPEN_PRS);
+
+      expect(loggerLogSpy).toHaveBeenCalledWith('Existing downloads: 4');
+      expect(loggerLogSpy).toHaveBeenCalledWith('Removing 2 download(s): 20-ABCDEF0-build.zip, 20-1234567-build.zip');
+    });
+
+
+    it('should construct full paths to directories (by prepending \'downloadsDir\')', () => {
+      cleaner.removeUnnecessaryDownloads(['dl-1', 'dl-2', 'dl-3'], []);
+
+      expect(shellRmSpy).toHaveBeenCalledWith(normalize('/downloads/dl-1'));
+      expect(shellRmSpy).toHaveBeenCalledWith(normalize('/downloads/dl-2'));
+      expect(shellRmSpy).toHaveBeenCalledWith(normalize('/downloads/dl-3'));
     });
 
 
     it('should remove the downloads that do not correspond to open PRs', () => {
       cleaner.removeUnnecessaryDownloads(EXISTING_DOWNLOADS, OPEN_PRS);
-      expect(shell.rm).toHaveBeenCalledTimes(2);
-      expect(shell.rm).toHaveBeenCalledWith('downloads/20-ABCDEF0-build.zip');
-      expect(shell.rm).toHaveBeenCalledWith('downloads/20-1234567-build.zip');
+      expect(shellRmSpy).toHaveBeenCalledTimes(2);
+      expect(shellRmSpy).toHaveBeenCalledWith(normalize('/downloads/20-ABCDEF0-build.zip'));
+      expect(shellRmSpy).toHaveBeenCalledWith(normalize('/downloads/20-1234567-build.zip'));
     });
 
-
-    it('should log the number of existing builds and builds to be removed', () => {
-      cleaner.removeUnnecessaryDownloads(EXISTING_DOWNLOADS, OPEN_PRS);
-
-      expect(loggerLogSpy).toHaveBeenCalledWith('Existing downloads: 4');
-      expect(loggerLogSpy).toHaveBeenCalledWith(
-        'Removing 2 download(s): downloads/20-ABCDEF0-build.zip, downloads/20-1234567-build.zip');
-    });
   });
 });